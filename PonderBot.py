f773c9fed68aa96a01909b1e9fd4ac5447cb2cdb
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2014-09-14 15:14:04 -0400

    Self checks and kill mech for the RaspiRobot board.

--- PonderBot.py
+++ PonderBot.py
@@ -11,5 +11,17 @@ database.db.test.find()
 camera = camera.Camera()
 camera.testCapture()
 
+#Initialize and run the RaspiRobot thread
 raspirobot = RaspiRobot()
-raspirobot.start()
+if raspirobot.okToRun == True:
+    raspirobot.start()
+else:
+    print "Can't use Raspi Robot Board. Quitting."
+    exit()
+
+from time import sleep
+sleep(1)
+raspirobot.kill()
+
+if bacon == True:
+    print "YES"
--- raspirobot.py
+++ raspirobot.py
@@ -5,16 +5,38 @@
 __author__ = 'andrew'
 import threading
 from rrb2 import *
+from time import sleep
+
+bacon = True
 
 class RaspiRobot(threading.Thread):
+
+    okToRun = False
 
     def __init__(self):
         super(RaspiRobot, self).__init__()
         print "Created Raspi Robot"
-        self.rr = RRB2()
+        try:
+            self.rr = RRB2()
+            self.rr.set_led1(True)
+            sleep(0.1)
+            self.rr.set_led2(True)
+            sleep(0.1)
+            self.rr.set_led1(False)
+            self.rr.set_led2(False)
+            self.okToRun = True
+            print "Raspi Robot Initialized."
+        except Exception:
+            self.okToRun = False
+            print "Failed to initialize RaspiRobot Board"
 
     def run(self):
         print "Running Raspi Robot"
-        self.rr.set_led1(True)
+        while self.okToRun == True:
+            print "OK."
+
+    def kill(self):
+        print "Killed Raspi Robot Thread."
+        self.okToRun = False
 
 
